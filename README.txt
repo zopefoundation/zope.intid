a0bdbfded7dabb25e02672024427f8c9649f65c6
Dan Korostelev <nadako@gmail.com>
2009-01-31 18:56:38 +0000

    Copy zope.app.intid to zope.intid.

---
+++ README.txt
@@ -0,0 +1,4 @@
+This package provides an API to create integer ids for any object. Later
+objects can be looked up by their id as well. This functionality is commonly
+used in situations where dealing with objects is undesirably, such as in
+search indices or any code that needs an easy hash of an object.
---
+++ setup.py
@@ -0,0 +1,66 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.intid package
+
+$Id$
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name = 'zope.app.intid',
+      version = '3.6.1dev',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      description='Integer Id Utility',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 integer id utility",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/zope.app.intid',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope', 'zope.app'],
+      extras_require = dict(test=['zope.app.testing',
+                                  'zope.app.securitypolicy',
+                                  'zope.site',
+                                  'zope.app.zcmlfiles']),
+      install_requires = ['setuptools',
+                          'ZODB3',
+                          'zope.container',
+                          'zope.app.keyreference',
+                          'zope.component',
+                          'zope.event',
+                          'zope.interface',
+                          'zope.location',
+                          'zope.security',
+                          ],
+      include_package_data = True,
+      zip_safe = False,
+      )
