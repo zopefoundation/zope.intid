1b7d2ba79c74056fbc88eead257c2d9ccb88d696
Stephan Richter <stephan.richter@gmail.com>
2013-02-22 21:55:04 -0500

    Back to development: 4.0.0a2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name = 'zope.intid',
-      version='4.0.0a1',
+      version='4.0.0a2.dev0',
       author='Albertas Agejevas and Gintautas Miliauskas',
       maintainer='Zope developers',
       maintainer_email='zope-dev@zope.org',
