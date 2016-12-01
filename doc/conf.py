8d350313327ba09b461739e2afda07ee179bebca
Jason Madden <jamadden@gmail.com>
2016-12-01 15:24:43 -0600

    Add link to zope.keyreference Sphinx docs. Also add missing docstrings.

--- doc/conf.py
+++ doc/conf.py
@@ -349,6 +349,7 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     'https://docs.python.org/': None,
+    'https://zopekeyreference.readthedocs.io/en/latest': None,
 }
 
 extlinks = {'issue': ('https://github.com/zopefoundation/zope.intid/issues/%s',
